d6e4a05b2462435a8f2a64b401c5eab584736a9e
Minsoyoonn <82926519+Minsoyoonn@users.noreply.github.com>
2021-11-10 00:23:23 +0900

    블로그완성

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,7 +5,7 @@ function App(){
     let [글제목, 글제목변경]=useState(["나은", "건후", "진우"]);
     let [누른버튼, 누른버튼변경] = useState(0);
     let [따봉, 따봉변경]=useState(0)
-
+    let [입력값, 입력값변경] = useState('');
     return (
         <div className="App">
             <div className="black-nav">
@@ -14,7 +14,7 @@ function App(){
             {
                 글제목.map((글, i)=>{
                     return (
-                        <div className="list">
+                        <div className="list" key={i}>
                             <h3 onClick={()=>{누른버튼변경(i)}}> {글} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h3>
                             <p> 2월 19일 발행 </p>
                             <hr/>
@@ -22,6 +22,16 @@ function App(){
                     )
                 })
             }
+
+            <div className="publish">
+              <input onChange={(e)=>(입력값변경(e.target.value))}/>
+              <button onClick={()=>{
+                var arrayCopy=[...글제목];
+                arrayCopy.unshift(입력값);
+                글제목변경(arrayCopy);
+              }}>저장</button>
+            </div>
+
             <Modal 글제목={글제목} 누른버튼={누른버튼}></Modal>
         </div>
     )
